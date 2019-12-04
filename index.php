f5225ae99326d6d731a4f4d305d5fe98d2406a13
Black <blackng10.5.1997@gmail.com>
2019-12-04 18:44:58 +0700

    Update

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+  define('DOC_ROOT', $_SERVER['DOCUMENT_ROOT']);
+  define('TITLE', 'HOME');
+  require_once DOC_ROOT.'common\php\common.php';
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <?php insertHeadInfo();  ?>
+    <?php insertCSS('/top/css/style.css');  ?>
+  </head>
+  <body>
+    <header>
+      <div class="Responsive">
+        <div class="Menu">
+          <div class="Menu-branch">
+            <div class="Menu-branch__logo">
+              <img src="/common/img/logo.webp" alt="">
+            </div>
+            <div class="Menu-branch-search">
+              <div class="Menu-branch-search-location">
+                <p class="Menu-branch-search-location_txt">Ha Noi</p>
+              </div>
+              <div class="Menu-branch-search-input">
+                <input type="text" placeholder="tim kiem">
+              </div>
+              <div class="Menu-branch-search-btn">
+                <i class="fa fa-search"></i>
+              </div>
+            </div>
+          </div>
+          <div class="Menu-list">
+
+          </div>
+
+        </div>
+      </div>
+    </header>
+  </body>
+</html>
