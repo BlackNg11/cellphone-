4c64ba582ecd99752bec9122b3b14855ed59b3e8
Black <blackng10.5.1997@gmail.com>
2019-12-05 13:51:48 +0700

    'update'

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <?php insertCSS('/top/css/style.css');  ?>
   </head>
   <body>
-    <header>
+    <header class="only-pc">
       <div class="Responsive">
         <div class="Menu">
           <div class="Menu-branch">
@@ -20,7 +20,11 @@
             </div>
             <div class="Menu-branch-search">
               <div class="Menu-branch-search-location">
-                <p class="Menu-branch-search-location_txt">Ha Noi</p>
+                <p class="js-locationBtn Menu-branch-search-location__txt">Ha Noi</p>
+                <div class="Menu-branch-search-location-select">
+                  <p class="js-locationOption"><i class="fa fa-map-marker"></i><span>Ha Noi</span></p>
+                  <p class="js-locationOption"><i class="fa fa-map-marker"></i><span>Ho Chi Minh</span></p>
+                </div>
               </div>
               <div class="Menu-branch-search-input">
                 <input type="text" placeholder="tim kiem">
@@ -85,7 +89,30 @@
         </div>
       </div>
     </header>
-
+    <header class="only-sp">
+      <div class="js-nav nav">
+        <div class="nav__logo">
+          <img src="\common\img\logo.webp" alt="cellphone">
+        </div>
+        <div class="nav-search">
+          <div class="nav-search-content">
+            <div class="nav-search__input">
+              <input type="text" placeholder="Tim Kiem">
+            </div>
+            <div class="nav-search__btn">
+              <i class="fa fa-search"></i>
+            </div>
+          </div>
+        </div>
+        <div class="nav-hambuger">
+          <div class="js-navHambuger nav-hambuger-content">
+            <span class="nav-hambuge-content__icon"></span>
+            <span class="nav-hambuge-content__icon"></span>
+            <span class="nav-hambuge-content__icon"></span>
+          </div>
+        </div>
+      </div>
+    </header>
     <?php insertJS('/top/js/script.js');  ?>
   </body>
 </html>
--- top/js/script.js
+++ top/js/script.js
@@ -1,5 +1,21 @@
 (function($) {
   $(function() {
+    $('.js-locationBtn').click(function() {
+      var mParent = $(this).parent();
 
+      mParent.toggleClass('Menu-branch-search-location--active');
+    });
+
+    $('.js-locationOption').click(function() {
+      var mLocation = $(this).find('span').text();
+      var locationBtn = $('.js-locationBtn');
+      locationBtn.text(mLocation);
+      locationBtn.parent().removeClass('Menu-branch-search-location--active');
+    });
+
+    $('.js-navHambuger').click(function() {
+      $('.js-nav').toggleClass('nav--active')
+
+    });
   })
 })(jQuery);
